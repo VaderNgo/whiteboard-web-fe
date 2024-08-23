71f88179327333f4949950fc1cede9783ffaf507
VaderNgo <vaderngo@gmail.com>
2024-08-23 13:54:56 +0700

    Sketch Dashboard UI

---
+++ src/app/(dashboard)/_components/navbar/index.tsx
@@ -0,0 +1,4 @@
+"use client";
+export const Navbar = () => {
+    return <div className="flex items-center gap-x-4 p-5 bg-green-500"></div>;
+};
---
+++ src/app/(dashboard)/_components/sidebar/index.tsx
@@ -0,0 +1,3 @@
+export const SideBar = () => {
+    return <aside className="fixed z-[1] left-0 bg-blue-500 h-full w-[60px] flex p-3 flex-col gap-y-4">Side</aside>;
+};
