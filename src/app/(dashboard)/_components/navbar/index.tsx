a649245b680d7132c03b84543592d88cacde5788
VaderNgo <vaderngo@gmail.com>
2024-08-23 21:27:36 +0700

    Finish search-input in navbar

--- src/app/(dashboard)/_components/navbar/index.tsx
+++ src/app/(dashboard)/_components/navbar/index.tsx
@@ -1,4 +1,14 @@
 "use client";
+
+import { SearchInput } from "./search-input";
+
 export const Navbar = () => {
-    return <div className="flex items-center gap-x-4 p-5 bg-green-500"></div>;
+    return (
+        <div className="flex items-center gap-x-4 p-5 ">
+            <div className="hidden lg:flex lg:flex-1">
+                <SearchInput />
+            </div>
+            {/* Avatar button here */}
+        </div>
+    );
 };
