42e0329ef70704259aed6b979e944e6ef5f6ef61
VaderNgo <vaderngo@gmail.com>
2024-08-23 21:07:47 +0700

    Sketch Org-Sidebar

--- src/app/(dashboard)/_components/org-sidebar/index.tsx
+++ src/app/(dashboard)/_components/org-sidebar/index.tsx
@@ -1,5 +1,27 @@
 "use client";
 
+import Link from "next/link";
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+
+const font = Poppins({
+    subsets: ["latin"],
+    weight: ["600"],
+});
+
 export const OrgSidebar = () => {
-    return <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5 bg-red-300">Organization Sidebar</div>;
+    return (
+        <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5 bg-red-300">
+            <Link href="/">
+                <div className="flex items-center gap-x-2">
+                    <Image
+                        alt="Logo"
+                        height={60}
+                        width={150}
+                        src="https://mirostatic.com/app/static/baa3028a5c006a0a.svg"
+                    />
+                </div>
+            </Link>
+        </div>
+    );
 };
