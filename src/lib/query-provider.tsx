6433001b2ef2eddce32cf3d8fadd158098e238d5
solux <loctranphuoc456@protonmail.com>
2024-09-17 21:23:29 +0700

    [Minor] Make code more readable

--- src/lib/query-provider.tsx
+++ src/lib/query-provider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, ReactNode } from "react";
+import { useState, useEffect, ReactNode } from "react";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { socket } from "./websocket";
 import { usePathname, useRouter } from "next/navigation";
@@ -9,31 +9,36 @@ export const QueryProvider = ({ children }: { children: ReactNode }) => {
   const router = useRouter();
   const pathname = usePathname();
   const [client] = useState(
-    new QueryClient({
-      defaultOptions: {
-        queries: {
-          staleTime: 600 * 1000,
-          gcTime: 1000 * 60 * 60 * 24,
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 600 * 1000,
+            gcTime: 1000 * 60 * 60 * 24,
+          },
         },
-      },
-    })
+      })
   );
 
-  socket.on("new_notification", () => {
+  const handleNewNotification = () => {
     client.invalidateQueries({ queryKey: ["notifications"] });
-  });
+  };
 
-  socket.on("team_member_updated", (teamId: any) => {
-    client.invalidateQueries({ queryKey: ["team-members", { teamId: teamId.toString() }] });
-  });
+  const handleTeamMemberUpdated = (teamId: string) => {
+    client.invalidateQueries({ queryKey: ["team-members", { teamId }] });
+  };
 
-  socket.on("removed_from_team", (teamId: any) => {
+  const handleRemovedFromTeam = (teamId: string) => {
     client.invalidateQueries({ queryKey: ["teams"] });
     if (pathname === `/dashboard/${teamId}`) {
       console.log("redirecting to dashboard");
       router.push("/dashboard");
     }
-  });
+  };
+
+  socket.on("new_notification", handleNewNotification);
+  socket.on("team_member_updated", handleTeamMemberUpdated);
+  socket.on("removed_from_team", handleRemovedFromTeam);
 
   return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
 };
