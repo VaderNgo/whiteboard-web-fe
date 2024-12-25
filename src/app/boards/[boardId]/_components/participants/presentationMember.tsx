28a8cfbb5d3b80c3b9c12e63bdc0103711aee486
VaderNgo <vaderngo@gmail.com>
2024-12-25 16:02:11 +0700

    Add presentationMember

---
+++ src/app/boards/[boardId]/_components/participants/presentationMember.tsx
@@ -0,0 +1,9 @@
+const PresentationMember = ({}) => {
+  return (
+    <div className="absolute z-10 h-12 bottom-2 right-[50%] bg-white rounded-e-md flex items-center shadow-md">
+      <div className="flex h-full w-full justify-center items-center space-x-3 p-2">
+        <div className="aspect-square h-full rounded flex justify-center items-center"></div>
+      </div>
+    </div>
+  );
+};
